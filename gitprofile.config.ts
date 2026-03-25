9b5f689b2c519ea387712a69437d1a12cb419aee
Jingsong Chen <jschen02@uw.edu>
2026-03-24 22:12:47 -0700

    Update gitprofile.config.ts resumeURL

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -88,7 +88,7 @@ const CONFIG = {
   },
   resume: {
     fileUrl:
-      'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf', // Empty fileUrl will hide the `Download Resume` button.
+      '/CV_Jingsong_Chen.pdf', // Empty fileUrl will hide the `Download Resume` button.
   },
   skills: [
     'Java',
